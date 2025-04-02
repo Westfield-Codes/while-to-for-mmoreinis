c29960d9b80b4203ec98936d39432811daedecd9
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-04-02 11:08:03 -0400

    Fixed guess-kata.js

--- guess-kata.js
+++ guess-kata.js
@@ -3,13 +3,13 @@
 // Define a variable answer, set equal to a random number between 1 and 10
 // Define a variable guess, set equal to 0
 // Define a variable guessCount, set equal to 0
-// Let the user guess WHILE count is less than 4
+// Let the user guess WHILE guessCount is less than 4
 // Add one to guessCount
 // Set guess equal to the user input after "Guess my number"
 // If guess is equal to answer, say "Right!" and break the loop
 // Otherwise say "Wrong!"
 // End the loop
-// If guessCount is equal to 3, say "Out of guesses. The number was ANSWER"
+// If guessCount is equal to 4, say "Out of guesses. The number was ANSWER"
 
 /* For Loop Conversion */
 
